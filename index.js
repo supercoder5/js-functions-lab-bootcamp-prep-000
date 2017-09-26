fe3c22644cec35dbcbc1a107edc1a8e487226807
Daniel Smith <the.dan.s.smith@gmail.com>
2017-09-26 20:40:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(holiday, name)  {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(days, holiday)  {
-  return `It's ${days} until ${holiday}!`
+  return `It\'s ${days} until ${holiday}!`
 }

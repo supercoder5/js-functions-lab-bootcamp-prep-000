f2917e70a97f2a8b0234f14b14fc7b67d170ad8a
Daniel Smith <the.dan.s.smith@gmail.com>
2017-09-26 20:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // write your code below!
-function happyHolidays()
+function happyHolidays()  {
   return `Happy holidays!`
+  }
+function happyHolidaysTo(name)  {
+  return 'Happy holidays, ${name}'
+}

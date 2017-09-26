b239e3adfee9afcbfdd185f2a4d45c6ea7c1e47e
Daniel Smith <the.dan.s.smith@gmail.com>
2017-09-26 20:39:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ function happyHolidays()  {
   return `Happy holidays!`
   }
 function happyHolidaysTo(name)  {
-  return 'Happy holidays, ${name}'
+  return `Happy holidays, ${name}`
+}
+function happyHolidayTo(holiday, name)  {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days, holiday)  {
+  return `It's ${days} until ${holiday}!`
 }
